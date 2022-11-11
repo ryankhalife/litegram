aa68822c6f69c1d5b1d011bf1b04b4082f9df51b
Ryan Khalife <khaliferyan@gmail.com>
2022-11-11 19:45:19 +0200

    Backend: created login.php

---
+++ litegram-backend/login.php
@@ -0,0 +1,45 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+header("Access-Control-Allow-Headers: X-Requested-With");
+
+include('connection.php');
+
+require __DIR__ . '/vendor/autoload.php';
+use Firebase\JWT\JWT;
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+// get user from database
+$query = $mysqli->prepare("SELECT * FROM users WHERE username = ?");
+$query->bind_param("s", $username);
+$query->execute();
+$result = $query->get_result();
+$user = $result->fetch_assoc();
+
+if ($user == null || !password_verify($password, $user['password'])) {
+    $response = [];
+    $response['success'] = false;
+    $response['message'] = "Incorrect username or password";
+
+    die(json_encode($response));
+}
+
+$response = [];
+$response['success'] = true;
+$response['message'] = "Login successful";
+
+$payload = [
+    "id" => $user['id'],
+    "username" => $user['username'],
+    "email" => $user['email'],
+    "f_name" => $user['f_name'],
+    "l_name" => $user['l_name'],
+];
+
+$jwt = JWT::encode($payload, $key, "HS256");
+$response['token'] = $jwt;
+
+echo json_encode($response);
