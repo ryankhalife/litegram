8122742e655b76eb04fc5f74671886b3b77a0075
Ryan Khalife <khaliferyan@gmail.com>
2022-11-18 18:59:02 +0200

    Backend: Created get_feed.php

---
+++ litegram-backend/get_feed.php
@@ -0,0 +1,44 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+header("Access-Control-Allow-Headers: X-Requested-With");
+
+include('connection.php');
+
+require __DIR__ . '/vendor/autoload.php';
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
+//Validate JWT
+$headers = apache_request_headers();
+$auth = $headers['Authorization'];
+$token = str_replace("Bearer ", "", $auth);
+
+try {
+    $decoded = JWT::decode($token, new Key($key, 'HS256'));
+    $user_id = $decoded->id;
+} catch (Exception $e) {
+    $response = [];
+    $response['success'] = false;
+    $response['message'] = "Invalid token";
+
+    die(json_encode($response));
+}
+
+// get posts not from user
+$query = $mysqli->prepare("SELECT * FROM images WHERE user_id != ? ORDER BY date DESC");
+$query->bind_param("i", $user_id);
+$query->execute();
+
+$result = $query->get_result();
+
+$response = [];
+$response['success'] = true;
+$response['posts'] = [];
+
+while ($post = $result->fetch_assoc()) {
+    $response['posts'] = $post;
+}
+
+echo json_encode($response);
