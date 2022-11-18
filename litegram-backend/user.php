3738e3315b0d3410a0211770d06c9e6a394ecbf2
Ryan Khalife <khaliferyan@gmail.com>
2022-11-18 18:59:15 +0200

    Backend: Created user.php

---
+++ litegram-backend/user.php
@@ -0,0 +1,22 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+header("Access-Control-Allow-Headers: *");
+
+include('connection.php');
+
+$id = $_GET['id'];
+
+$query = $mysqli->prepare("SELECT * FROM users WHERE id = ?");
+$query->bind_param("i", $id);
+$query->execute();
+
+$result = $query->get_result();
+$user = $result->fetch_assoc();
+
+$response = [];
+$response['success'] = true;
+$response['user'] = $user;
+
+echo json_encode($response);
