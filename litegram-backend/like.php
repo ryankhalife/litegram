06f3eb2bddaba0051b3e553c250171bc863eda78
Ryan Khalife <khaliferyan@gmail.com>
2022-11-18 22:20:31 +0200

    Backend: Fixed cors error with like.php

--- litegram-backend/like.php
+++ litegram-backend/like.php
@@ -2,7 +2,7 @@
 
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST');
-header("Access-Control-Allow-Headers: X-Requested-With");
+header("Access-Control-Allow-Headers: *");
 
 include('connection.php');
 
